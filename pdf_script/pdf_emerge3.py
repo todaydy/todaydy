64158bc24fb7500a763e36ec29445f032befaead
todaydy <77233451+todaydy@users.noreply.github.com>
2022-03-25 20:42:01 +0800

    Delete pdf_emerge3.py

--- pdf_script/pdf_emerge3.py
+++
@@ -1,9 +0,0 @@
-from PyPDF2 import PdfFileMerger, PdfFileReader
-from glob import glob
-print(sorted(glob(pathname="pdf/*.pdf")))
-
-mergepdf = PdfFileMerger()
-for temp in sorted(glob(pathname="pdf/*.pdf")):
-#for temp in b:
-   mergepdf.append(PdfFileReader(temp))
-mergepdf.write("all.pdf")
